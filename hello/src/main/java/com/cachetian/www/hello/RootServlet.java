6ab78f9bca4e808fcc93588e73905b821ccc633b
tianmu <hit.mutian@qq.com>
2018-06-02 19:58:09 +0800

    Update app hello.app solve path overlap issue.
    Chage / to /root

--- hello/src/main/java/com/cachetian/www/hello/RootServlet.java
+++ hello/src/main/java/com/cachetian/www/hello/RootServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/root")
 public class RootServlet extends HttpServlet {
 	private static final long serialVersionUID = 9181283319629364771L;
 
